ab774e6cea01780fd1023b01b0ea4680d7990783
Jay Butera <buterajay@gmail.com>
2015-03-01 07:15:52 -0600

    Fix bugs in LowFareForecast

--- src/LowFareForecast.py
+++ src/LowFareForecast.py
@@ -1,11 +1,16 @@
+import HTTPCall
+
 class LowFareForecast(object):
-    def __int__(self):
+    def __init__(self):
+        self.HandleREST = HTTPCall.HTTPCall()
+        self.HandleREST.request_authentication()
+
         self.tasks = {  \
             'origin'        : ['origin=', False], \
             'destination'   : ['destination=', False],\
             'departuredate' : ['departuredate=', False],\
             'returndate'    : ['returndate=', False]}
-            
+
         self.response = {}
 
     def origin_location(self):
@@ -24,19 +29,19 @@ class LowFareForecast(object):
         Returns a string
         '''
         return self.response['DepartureDateTime']
-    
+
     def return_date_time(self):
         '''
         Returns a string
         '''
         return self.response['ReturnDateTime']
-    
+
     def highest_predicted_fare(self):
         '''
-        Returns an int 
+        Returns an int
         '''
         return self.response['highestPredictedFare']
-    
+
     def lowest_predicted_fare(self):
         '''
         Returns an int
@@ -83,25 +88,29 @@ class LowFareForecast(object):
         Adding the user input to departuredate string 
         '''
         self.tasks['departuredate'][1] = True
-        self.tasks['departuredate'][0] += ','.join(depart)
+        self.tasks['departuredate'][0] += depart
+        #self.tasks['departuredate'][0] += '{0}-{1}-{2}'.format( \
+        #        depart[:3], depart[4:5], depart[5:6])
 
     def returndate(self, rdate):
         '''
         Adding the user input to returndate string 
         '''
         self.tasks['returndate'][1] = True
-        self.tasks['returndate'][0] += ','.join(rdate)
+        self.tasks['returndate'][0] += rdate
+        #self.tasks['returndate'][0] += '{0}-{1}-{2}'.format( \
+        #        rdate[:3], rdate[4:5], rdate[5:6])
 
     ### Call Function ###
 
     def call(self):
-        if self.tasks['departuredate'][1]:
-            assert self.tasks['lengthofstay'][0].count(',') < 5
-            else:
-                assert self.tasks['lengthofstay'][0].count(',') < 10
+        # All tasks are required
+        assert (False not in self.tasks.values()[1])
 
-        self.response = self.HandleREST.request_content( '/v1/forecast/flights/fares?' + \
+        print ('/v1/forecast/flights/fares?' + \
                     '&'.join([task[0] for task in self.tasks.values() if task[1]]))
-        
+        self.response = self.HandleREST.request_content('/v1/forecast/flights/fares?' + \
+                    '&'.join([task[0] for task in self.tasks.values() if task[1]]))
+
         # Return JSON content
         return self.response
