5e46986c381e699d5adfa200da0e188d9e89af26
Jay Butera <buterajay@gmail.com>
2015-03-01 06:17:46 -0600

    Fill config URLs

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 clientID = ''
 password = ''
 
-auth_url  = 'https://api.sabre.com'
-sabre_url = 'https://api.sabre.com'
+auth_url  = 'https://api.test.sabre.com/v1/auth/token'
+sabre_url = 'https://api.test.sabre.com'
 
 access_token = ''
